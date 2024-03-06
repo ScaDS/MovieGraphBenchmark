642b77132b411b428c926ddd7ad9ee1c06cedef7
Daniel Obraczka <daniel@obraczka.de>
2024-03-06 11:44:37 +0100

    Remove commented out-code

--- src/moviegraphbenchmark/get_imdb_data.py
+++ src/moviegraphbenchmark/get_imdb_data.py
@@ -8,7 +8,6 @@ from moviegraphbenchmark.utils import download_file
 uris = {
     "https://web.archive.org/web/20200717014821/https://datasets.imdbws.com/name.basics.tsv.gz": "name.basics.tsv",
     "https://web.archive.org/web/20200717014801/https://datasets.imdbws.com/title.basics.tsv.gz": "title.basics.tsv",
-    # "https://datasets.imdbws.com/title.crew.tsv.gz": "title.crew.tsv",
     "https://web.archive.org/web/20200717014920/https://datasets.imdbws.com/title.episode.tsv.gz": "title.episode.tsv",
     "https://web.archive.org/web/20200717014706/https://datasets.imdbws.com/title.principals.tsv.gz": "title.principals.tsv",
 }
